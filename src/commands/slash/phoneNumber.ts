bb22347a3c50c376259e67de125df37e816888be
Murali Anand <71955467+muralianand12345@users.noreply.github.com>
2024-07-01 23:34:13 +0530

    💠2.3.1 - dev

--- src/commands/slash/phoneNumber.ts
+++ src/commands/slash/phoneNumber.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, EmbedBuilder, TextChannel } from "discord.js";
+import { SlashCommandBuilder, EmbedBuilder, TextChannel, Message } from "discord.js";
 
 import { SlashCommand, IPhoneNumber } from "../../types";
 import PhoneModal from "../../events/database/schema/phoneNumber";
@@ -44,8 +44,73 @@ const command: SlashCommand = {
         const type = interaction.options.getString('type');
         if (!type) return interaction.editReply({ content: 'Please select a type of phone number.' });
 
-        await interaction.editReply({ content: 'Under Development' });
+        const collectorFilter = (message: Message) => message.author.id === interaction.user.id;
+        await interaction.editReply({ content: 'Please provide the phone number you want to set.' }).then(async () => {
+            await interaction.channel?.awaitMessages({ filter: collectorFilter, max: 1, time: 60000, errors: ['time'] })
+                .then(async (collected: any) => {
+                    await interaction.editReply({ content: 'Processing your request...' });
 
+                    const phoneNumber = collected.first().content;
+
+                    if (!phoneNumber || isNaN(Number(phoneNumber))) {
+                        return await interaction.editReply({ content: 'Please provide a valid phone number!' });
+                    }
+                    if (phoneNumber.length < 4 || phoneNumber.length > 10) {
+                        return await interaction.editReply({ content: 'Phone number must be between 4 and 10 digits.' });
+                    }
+
+                    switch (type) {
+                        case 'identical':
+                            const allIdentical = phoneNumber.split('').every((digit: string) => digit === phoneNumber[0]);
+                            if (!allIdentical) {
+                                return await interaction.editReply({ content: 'Phone number must have all identical digits for the "identical" type.' });
+                            }
+                            break;
+                        case 'running':
+                            const isAscending = phoneNumber.split('').every((digit: string, index: number, arr: string[]) => {
+                                return index === 0 || (digit.charCodeAt(0) - arr[index - 1].charCodeAt(0) === 1);
+                            });
+                            const isDescending = phoneNumber.split('').every((digit: string, index: number, arr: string[]) => {
+                                return index === 0 || (arr[index - 1].charCodeAt(0) - digit.charCodeAt(0) === 1);
+                            });
+                            if (!isAscending && !isDescending) {
+                                return await interaction.editReply({ content: 'Phone number must be in a running sequence for the "running" type.' });
+                            }
+                            break;
+                        default:
+                            return await interaction.editReply({ content: 'Please provide a valid phone number type!' });
+                    }
+
+                    const phoneData = await PhoneModal.findOne({
+                        phonenumber: Number(phoneNumber)
+                    });
+                    if (phoneData) {
+                        return await interaction.editReply({ content: 'This phone number is already in use!' });
+                    }
+
+                    const newPhoneData: IPhoneNumber = new PhoneModal({
+                        userId: interaction.user.id,
+                        phonenumber: Number(phoneNumber),
+                        status: true
+                    });
+
+                    await newPhoneData.save().then(async () => {
+                        embed.setColor('Green')
+                            .setDescription('Phone number has been successfully set!');
+                        return await interaction.editReply({ content: "", embeds: [embed] });
+                    }).catch(async (err) => {
+                        embed.setColor('Red')
+                            .setDescription('An error occurred while setting the phone number!');
+                        return await interaction.editReply({ content: "", embeds: [embed] });
+                    });
+                })
+                .catch(async (error) => {
+                    if (error as Error && error.message === 'time') {
+                        return await interaction.editReply({ content: 'Time expired. Please try setting your phone number again.' });
+                    }
+                    return await interaction.editReply({ content: 'An error occurred while processing your request.' });
+                });
+        });
     }
 };
 
