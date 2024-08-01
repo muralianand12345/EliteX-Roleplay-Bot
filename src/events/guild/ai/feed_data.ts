013d3b2acbc8c78e9f492969579fcccc50167385
Murali Anand <71955467+muralianand12345@users.noreply.github.com>
2024-08-01 12:39:39 +0530

    💠2.6.0.7

--- src/events/guild/ai/feed_data.ts
+++ src/events/guild/ai/feed_data.ts
@@ -1,4 +1,4 @@
-import { Events, Message, Client, TextChannel, VoiceChannel, ThreadChannel, NewsChannel, User } from "discord.js";
+import { Events, Message, Client, GuildTextBasedChannel, User } from "discord.js";
 import { config } from "dotenv";
 import { processNewData } from "../../../utils/ai/ai_functions";
 
@@ -14,10 +14,12 @@ const event: BotEvent = {
         const channel_data_list = client.config.ai.channel_data_list;
         if (!channel_data_list || !channel_data_list.includes(message.channel.id) || message.author.bot) return;
 
-        const msg_channel = message.channel as  TextChannel | VoiceChannel | ThreadChannel | NewsChannel;
+        const msg_channel = message.channel as GuildTextBasedChannel;
         const msg_user = message.author as User;
+        const channelTopic = 'topic' in msg_channel ? msg_channel.topic : undefined;
 
         const channel_data = `
+### ${channelTopic || msg_channel.name} ###
 **Message From Channel**: [Name] -> (${msg_channel.name}) [Id] -> (${msg_channel.id}) [Type] -> (${msg_channel.type})
 **Message From User**: [Tag] -> (${msg_user.tag}) [Username] -> (${msg_user.username}) [Id] -> (${msg_user.id})
 **Message Content**:
